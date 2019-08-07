7bb1ed33b8f47280864ba66a9932f3e389d93ac6
Josh Montoya <josh@montoya.io>
2019-08-07 14:48:27 -0700

    Improve commenting

--- ignore.go
+++ ignore.go
@@ -13,20 +13,31 @@ func getIgnored() (ignored *stringset.StringSet) {
 		err error
 	)
 
+	// Attempt to open ignore file
 	if f, err = os.Open(".branch-releaser-ignore"); err != nil {
+		// Ignore file does not exist. This file is not required, so we do not need to throw an error.
 		return
 	}
+	// Defer the closing of the file
 	defer f.Close()
 
+	// Initialize new stringset
 	ignored = stringset.New()
+	// Initialize new scanner
 	scn := bufio.NewScanner(f)
 
+	// Iterate through file lines
 	for scn.Scan() {
+		// Assign bytes to variable
 		bs := scn.Bytes()
+
+		// Check byte length
 		if len(bs) == 0 {
+			// Ignore empty lines, continue
 			continue
 		}
 
+		// Set current line contents as a stringset key
 		ignored.Set(string(bs))
 	}
 
